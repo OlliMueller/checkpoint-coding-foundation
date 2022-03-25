6cf213196a5f7a17823f3c398dc9561804b707ca
Oliver Müller <oriba.myura@gmail.com>
2022-03-25 11:16:24 +0100

    final commit

--- sum-app.js
+++ sum-app.js
@@ -1,3 +1,25 @@
+// use constructor-method with this.numbers-property...
+// to make it available in all 3 following methods (addNumber, getSum & reset)
 class SumApp {
-  // Add you solution here
+  constructor() {
+    this.numbers = [];
+  }
+
+  //push a "number" into the array each time addNumber is executed
+  addNumber(number) {
+    this.numbers.push(number);
+  }
+
+  // take an array of numbers, reduce them to the sum; default (startValue)= 0
+  getSum() {
+    return this.numbers.reduce(
+      (previousValue, currentValue) => previousValue + currentValue,
+      0
+    );
+  }
+
+  // delete all numbers previously added to the array
+  reset() {
+    this.numbers = [];
+  }
 }
