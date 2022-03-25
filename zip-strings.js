7d5ece32ff26570dcd5c84485bd0aa8c5c5ea940
Oliver Müller <oriba.myura@gmail.com>
2022-03-25 07:56:33 +0100

    initial commit using string.split and array.join with for-loops

--- zip-strings.js
+++ zip-strings.js
@@ -1,3 +1,29 @@
+// Variables for testing
+// let strA = "abc";
+// let strB = "123";
+
 function zipStrings(strA, strB) {
-  // Add you solution here
+  //split strA & strB / returns two arrays
+  let letterArray = strA.split("");
+  let numberArray = strB.split("");
+  let zippedArray = [];
+
+  //using array.push in two for-loops
+  if (letterArray.length >= numberArray.length) {
+    for (let i = 0; i < letterArray.length; i++) {
+      zippedArray.push(letterArray[i]);
+      zippedArray.push(numberArray[i]);
+    }
+  } else {
+    for (let i = 0; i < numberArray.length; i++) {
+      zippedArray.push(letterArray[i]);
+      zippedArray.push(numberArray[i]);
+    }
+  }
+  // merge items with array.join() and return it as a string
+  let zippedString = zippedArray.join("");
+  return zippedString;
 }
+
+// execute for testing
+zipStrings(strA, strB);
