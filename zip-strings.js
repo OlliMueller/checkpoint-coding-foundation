cbc09e52a9db3e4bbe38c36a4a24fdc331627a51
Oliver Müller <oriba.myura@gmail.com>
2022-03-25 08:14:45 +0100

    refactor: add ternary operators (instead of two for loops)

--- zip-strings.js
+++ zip-strings.js
@@ -1,29 +1,28 @@
 // Variables for testing
-// let strA = "abc";
-// let strB = "123";
+let strA = "abc";
+let strB = "123";
 
 function zipStrings(strA, strB) {
-  //split strA & strB / returns two arrays
+  //split strA & strB, returns two arrays
   let letterArray = strA.split("");
   let numberArray = strB.split("");
   let zippedArray = [];
 
-  //using array.push in two for-loops
-  if (letterArray.length >= numberArray.length) {
-    for (let i = 0; i < letterArray.length; i++) {
-      zippedArray.push(letterArray[i]);
-      zippedArray.push(numberArray[i]);
-    }
-  } else {
-    for (let i = 0; i < numberArray.length; i++) {
-      zippedArray.push(letterArray[i]);
-      zippedArray.push(numberArray[i]);
-    }
+  //using Ternary Operators for if/else-statement
+  const arrayLength =
+    letterArray.length >= numberArray.length
+      ? letterArray.length
+      : numberArray.length;
+
+  for (let i = 0; i < arrayLength; i++) {
+    zippedArray.push(letterArray[i]);
+    zippedArray.push(numberArray[i]);
   }
-  // merge items with array.join() and return it as a string
+
+  // merge items with array.join(), return it as a string
   let zippedString = zippedArray.join("");
   return zippedString;
 }
 
-// execute for testing
+// execute with Variables in line 2, 3 for testing
 zipStrings(strA, strB);
