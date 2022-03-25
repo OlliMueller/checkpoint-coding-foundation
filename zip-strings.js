51b21403af7c7c85ccbf3da557d0ec43c1eb8599
Oliver Müller <oriba.myura@gmail.com>
2022-03-25 08:28:47 +0100

    refactor: remove test variables and let variable names

--- zip-strings.js
+++ zip-strings.js
@@ -1,12 +1,8 @@
-// Variables for testing
-let strA = "abc";
-let strB = "123";
-
 function zipStrings(strA, strB) {
   //split strA & strB, returns two arrays
-  let letterArray = strA.split("");
-  let numberArray = strB.split("");
-  let zippedArray = [];
+  letterArray = strA.split("");
+  numberArray = strB.split("");
+  zippedArray = [];
 
   //using Ternary Operators for if/else-statement
   const arrayLength =
@@ -20,9 +16,6 @@ function zipStrings(strA, strB) {
   }
 
   // merge items with array.join(), return it as a string
-  let zippedString = zippedArray.join("");
+  zippedString = zippedArray.join("");
   return zippedString;
 }
-
-// execute with Variables in line 2, 3 for testing
-zipStrings(strA, strB);
